5aca585c641010d93b8a01579b61541e79431050
Nelson Bryan <201913582B@utea.edu.pe>
2022-11-22 21:40:04 -0500

    version final

--- tests/agua.php
+++ tests/agua.php
@@ -3,13 +3,13 @@
 class Agua{
     function getEstado(float $temperatura): string{
         
-        //if(is_float($temperatura) || is_int($temperatura)){
+        if(is_float($temperatura) || is_int($temperatura)){
             if($temperatura <=0) return 'Solido';
             if(0< $temperatura && $temperatura <100) return 'Liquido';
             if(100<=$temperatura ) return 'Gaseoso';
-        /*}
+        }
         else{
             return NULL;
-        }*/
+        }
     }
 }
